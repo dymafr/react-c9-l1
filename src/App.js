81a050528e4d776d0ce760ba1a6565a7e91622de
dymafr <erwan1309@hotmail.com>
2022-06-23 12:14:25 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
 import React from 'react';
 
-function handleClick(e) {
-  e.stopPropagation();
-  e.preventDefault();
-  console.log('CLIC', e);
-}
-
-function handleInput(e) {
-  console.log('INPUT', e);
-}
-
-function handleFocus(e) {
-  console.log('FOCUS', e);
-}
-
-function handleClickDiv() {
-  console.log('CLIC DIV');
-}
-
 function App() {
-  return (
-    <div
-      onClick={handleClickDiv}
-      className="d-flex flex-column justify-content-center align-items-center p-20"
-    >
-      <button onClick={handleClick} className="mb-20">
-        Submit
-      </button>
-      <input type="text" onInput={handleInput} onInput={handleFocus} />
-    </div>
-  );
+  return <h1>Hello</h1>;
 }
 
 export default App;
