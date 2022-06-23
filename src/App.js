a7ab598fd8e13133bb1ba5e7a3c323558da1fadc
dymafr <erwan1309@hotmail.com>
2022-06-23 12:21:58 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import AddTodo from './components/AddTodo';
 function App() {
   return (
     <div className="d-flex justify-content-center align-items-center p-20">
-      <div className="card p-20">
+      <div className="card container p-20">
         <h1 className="mb-20">Liste de tâches</h1>
         <AddTodo />
         <TodoList />
